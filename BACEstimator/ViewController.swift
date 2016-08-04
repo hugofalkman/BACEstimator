8db64b8ad2794cb5a984f28068e23c8cf455af32
hugofalkman <zilla@hs-macbook-pro.local>
2016-08-04 23:50:04 +0200

    Initial Commit

---
+++ BACEstimator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BACEstimator
+//
+//  Created by H Hugo Falkman on 2016-08-04.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
