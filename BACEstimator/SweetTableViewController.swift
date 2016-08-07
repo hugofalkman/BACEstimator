5adac49c45e18056240d795ba56a756bc59f067e
hugofalkman <zilla@hs-macbook-pro.local>
2016-08-07 20:32:21 +0200

    Receiving from Firebase complete

--- BACEstimator/SweetTableViewController.swift
+++ BACEstimator/SweetTableViewController.swift
@@ -13,13 +13,31 @@ import FirebaseAuth
 class SweetTableViewController: UITableViewController {
 
     var dbRef: FIRDatabaseReference!
+    var sweets = [Sweet]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dbRef = FIRDatabase.database().reference().child("sweet-items")
-        
+        startObservingDB()
      }
 
+    func startObservingDB() {
+        dbRef.observeEventType(.Value, withBlock: { (snapshot: FIRDataSnapshot) in
+            var newSweets = [Sweet]()
+            
+            for sweet in snapshot.children {
+                let sweetObject = Sweet(snapshot: sweet as! FIRDataSnapshot)
+                newSweets.append(sweetObject)
+            }
+            
+            self.sweets = newSweets
+            self.tableView.reloadData()
+            
+        }) { (error: NSError) in
+                print(error.description)
+        }
+    }
+    
     @IBAction func addSweet(sender: UIBarButtonItem) {
         let sweetAlert = UIAlertController(title: "New Sweet", message: "Enter your Sweet", preferredStyle: .Alert)
         sweetAlert.addTextFieldWithConfigurationHandler { (textField: UITextField) in
@@ -46,14 +64,25 @@ class SweetTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return sweets.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
+        let sweet = sweets[indexPath.row]
         
+        cell.textLabel?.text = sweet.content
+        cell.detailTextLabel?.text = sweet.addedByUser
 
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            
+            let sweet = sweets[indexPath.row]
+            sweet.itemRef?.removeValue()
+        }
     }
 }
